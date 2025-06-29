d919ec71c15498a15dc99b999719dffcdbf8af8d
TheLogicle <70608147+TheLogicle@users.noreply.github.com>
2025-06-28 21:22:57 -0400

    addingtrouble problem

---
+++ addingtrouble/addingtrouble.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main(){
+	int64_t a, b, c;
+	cin >> a >> b >> c;
+
+	if(a + b == c) cout << "correct!" << endl;
+	else cout << "wrong!" << endl;
+}
